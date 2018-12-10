cdf456670eab0d63bad332b764f0946d5cf56261
ristp <rishen.t.pillai@gmail.com>
2018-12-10 10:37:26 +0400

    Algorithm revised and is now for efficient. And, I added function added.

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,19 @@
-var numbers = [22, 4,150, 4, 16, 25, 16, 53, 150, 6];
+var numbers = [22, 4, 150, 45, 1558, 16, 25, 16, 53, 150, 6];
 var tempVariable, tempVariable2, j;
 
-console.log(numbers);
+console.log("List of numbers before sorting: " + numbers);
 
 for (var j = 0; j < numbers.length; j++) {
   for (i = 0; i < numbers.length; i++) {
-    if (numbers[i] > numbers[i + 1]) {
-      tempVariable2 = numbers[i + 1];
-      tempVariable = numbers[i];
-      numbers[i] = tempVariable2;
-      numbers[i + 1] = tempVariable;
-    }
+    sort();
   }
 }
-console.log(numbers);
+
+function sort() {
+  if (numbers[i] > numbers[i + 1]) {
+    tempVariable = numbers[i + 1];
+    numbers[i + 1] = numbers[i];
+    numbers[i] = tempVariable;
+  }
+}
+console.log("List of numbers after sorting: " + numbers);
