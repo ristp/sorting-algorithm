47447edd2a4b5acc848efe0c8643f8be25afb924
ristp <rishen.t.pillai@gmail.com>
2018-12-10 09:36:23 +0400

    Added some new test values

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,9 @@
-var numbers = [22, 4, 25, 16, 53, 150, 6];
-var tempVariable, tempVariable2,j;
+var numbers = [22, 4,150, 4, 16, 25, 16, 53, 150, 6];
+var tempVariable, tempVariable2, j;
 
 console.log(numbers);
 
-for (var j = 0; j < numbers.length; j++){
+for (var j = 0; j < numbers.length; j++) {
   for (i = 0; i < numbers.length; i++) {
     if (numbers[i] > numbers[i + 1]) {
       tempVariable2 = numbers[i + 1];
@@ -13,6 +13,4 @@ for (var j = 0; j < numbers.length; j++){
     }
   }
 }
-  
-
-
+console.log(numbers);
